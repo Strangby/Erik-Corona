06b68d96c88903a30803e35f759468c08baa910e
ainz99899 <samuel@battleaxe.se>
2024-12-03 16:07:55 +0100

    w

--- Assets/Scripts/Sjukdom.cs
+++ Assets/Scripts/Sjukdom.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Sjukdom : MonoBehaviour
 {
+    public int speed = 5;
+    public bool state;
+    private Vector3 Target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Target = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, Target, speed * Time.deltaTime);
+        if (transform.position == Target)
+        {
+            Target = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
+        }
+    }
+    IEnumerator IDLE()
+    {
+
+
+        yield return null;
     }
 }
