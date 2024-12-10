bd0603a1d715c7403314e58efebcbc1b9aa17d60
ainz99899 <samuel@battleaxe.se>
2024-12-10 16:07:59 +0100

    JA

--- Assets/Scripts/Sjukdom.cs
+++ Assets/Scripts/Sjukdom.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Sjukdom : MonoBehaviour
 {
     public int speed = 5;
-    public bool state;
+    private bool state;
     private Vector3 Target;
+    private int Dice;
+    string status = "Healthy";
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +26,44 @@ public class Sjukdom : MonoBehaviour
         {
             Target = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
         }
+        Dice = Random.Range(0, 100);
+        if (Dice <= 20)
+        {
+            status = "Sick";
+        }
+        if (Dice >= 90)
+        {
+            status = "Immune";
+        }
+        if (Dice >= 21 && Dice <= 89)
+        {
+            status = "Healthy";
+        }
+        if (status == "Healthy")
+        {
+            
+        }
+        if (status == "Sick")
+        {
+
+        }
+        if (status == "Immune")
+        {
+
+        }
+        if (status == "Dead")
+        {
+
+        }
+    }
+    
+    IEnumerator Timer()
+    {
+
+        yield return null;
     }
     IEnumerator IDLE()
     {
-
 
         yield return null;
     }
