56f108aba66b06d0e5f73f5c62b29fbfc24d166c
ainz99899 <samuel@battleaxe.se>
2024-12-17 08:36:18 +0100

    JA

--- Assets/Scripts/Sjukdom.cs
+++ Assets/Scripts/Sjukdom.cs
@@ -8,6 +8,7 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 public class Sjukdom : MonoBehaviour
 {
     public int speed = 5;
+    public float targetTime = 10.0f;
     private bool state;
     private Vector3 Target;
     private int Dice;
@@ -22,35 +23,48 @@ public class Sjukdom : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, Target, speed * Time.deltaTime);
+        
         if (transform.position == Target)
         {
             Target = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
         }
-        Dice = Random.Range(0, 100);
-        if (Dice <= 20)
+        targetTime -= Time.deltaTime;
+
+        if (targetTime == 0.0f)
         {
-            status = "Sick";
+            Dice = Random.Range(0, 100);
+            
+            if (Dice <= 20)
+            {
+                status = "Sick";
+            }
+            
+            if (Dice >= 90)
+            {
+                status = "Immune";
+            }
+            
+            if (Dice >= 21 && Dice <= 89)
+            {
+                status = "Healthy";
+            }
         }
-        if (Dice >= 90)
-        {
-            status = "Immune";
-        }
-        if (Dice >= 21 && Dice <= 89)
-        {
-            status = "Healthy";
-        }
+        
         if (status == "Healthy")
         {
             
         }
+        
         if (status == "Sick")
         {
 
         }
+        
         if (status == "Immune")
         {
 
         }
+        
         if (status == "Dead")
         {
 
